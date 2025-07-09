ff9eda7eecccfce4326093c8828a000f24c23e1f
AKSHAYAS03 <AKSHAYAS03>
2025-07-09 15:51:19 +0530

    DSA code

--- dsa/src/main/java/sjb/dsa/SinglyLinkedList.java
+++ dsa/src/main/java/sjb/dsa/SinglyLinkedList.java
@@ -2,11 +2,11 @@ package sjb.dsa;
 
 public class SinglyLinkedList {
 	Node head;
-	
+//	ENDING POSITION
 	public void insert(int data) {
 		Node newNode=new Node(data);
 		
-		
+	
 		if(head==null) {
 			head=newNode;
 		}
@@ -27,14 +27,49 @@ public class SinglyLinkedList {
 		}
 		System.out.println("null");
 		}
-public static void main(String [] args) {
+public static void main1(String [] args) {
 	SinglyLinkedList list=new SinglyLinkedList();
 	list.insert(10);
 	list.insert(20);
 	list.insert(30);
-	list.insert(40);
+	list.addatposition(90, 2);
 	list.display();
 }
-	
-}
 
+//BEGINNING POSITION
+	public void insert1(int data)
+	{
+		Node newNode=new Node(data);
+		newNode.next=head;
+		head=newNode;
+}
+	public void display1() {
+		Node current=head;
+		
+		while(current !=null) {
+			System.out.println(current.data);
+			current=current.next;
+	}
+	System.out.println("null");	
+	}
+		public static void main(String [] args) {
+		SinglyLinkedList list=new SinglyLinkedList();
+		list.insert(10);
+		list.insert(20);
+		list.insert(30);
+		list.addatposition(90, 2);
+        list.display();
+		}
+//at specific position
+public void addatposition(int data,int pos)
+{
+	Node newNode=new Node(data);
+	Node temp=head;
+	for(int i=0;i<pos-1 && temp.next!=null;i++) {
+		temp=temp.next;
+		
+	}
+	newNode.next=temp.next;
+	temp.next=newNode;
+	}
+}
